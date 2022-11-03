311c4a80d4d53890f256af29aa8ea1ee1cefb954
jpgonzalez <jpgonzalez@106-11.info2>
2022-11-03 14:06:00 +0100

    Ejercicios tema1

--- EjerciciosTema1/src/ejerciciostema1/Ejercicio1.java
+++ EjerciciosTema1/src/ejerciciostema1/Ejercicio1.java
@@ -1,9 +1,23 @@
 package ejerciciostema1;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		
+		double num;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Dime un número: ");
+		num = sc.nextDouble();
+		
+		num += 0.5;
+		
+		System.out.print("El número redondeado sería: " + (int) num);
+		
+		sc.close();
 
 	}
 
